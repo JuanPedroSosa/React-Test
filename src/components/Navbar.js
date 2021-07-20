20f7feeb0692c2a02b51fdddd3d18d4dda4d0f85
Juan Pedro Sosa <juanpedrososa@outlook.com>
2021-07-19 22:24:57 -0300

    useHistory agregado

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 // <a className="navbar-brand text-white" href="#">App Midex</a>
-export const Navbar = props => (
+// <Link to="/" className="btn btn-primary text-white">Cerrar</Link>
+export const Navbar = props => {
+	let history = useHistory();
+
+return(
 	<nav className="navbar navbar-expand-lg navbar-light bg-danger">
   <div className="container-fluid">
     <a className="navbar-brand text-white">App Midex</a>
 
       <form className="d-flex">
-				<Link to="/" className="btn btn-primary text-white">Cerrar</Link>
+			<button onClick={history.goBack} className="btn btn-primary text-white">Cerrar</button>
+
       </form>
 
   </div>
 </nav>
 );
-
+}
